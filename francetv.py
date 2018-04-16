48a8daf8f9dc3b76cd8e563c17e6e7ce992807dc
SlyWink <slydev@openaliasbox.org>
2018-04-16 21:36:54 +0200

    Manager empty result list

--- francetv.py
+++ francetv.py
@@ -27,34 +27,37 @@ else:
     if emission['titre'].lower().find(opt.keyword.lower()) != -1:
       liste.append(emission)
 
-  for cpt in range(len(liste)):
-    em = liste[cpt]
-    print(u'{0}) [{1} {2}] : {3} ({4})'.format(cpt+1,em['date_diffusion'].replace('T',' '),em['chaine_id'],em['titre'],em['soustitre']))
-  print("0) QUITTER")
+  if len(liste) == 0:
+    print(u'Aucune émission trouvée !')
+  else:
+    for cpt in range(len(liste)):
+      em = liste[cpt]
+      print(u'{0}) [{1} {2}] : {3} ({4})'.format(cpt+1,em['date_diffusion'].replace('T',' '),em['chaine_id'],em['titre'],em['soustitre']))
+    print("0) QUITTER")
 
-  while True:
-    indice = int(input("Choix => "))
-    if indice == 0:
-      break
-    if indice <= len(liste):
-      js = requests.get(URL2.format(liste[indice-1]['id_diffusion'])).json()
-      url = ''
-      for vid in js['videos']:
-        if vid['format'] == 'm3u8-download':
-          url = vid['url']
-          break
-      liste_m3u8 = requests.get(url).text
-      for m3u8 in liste_m3u8.splitlines():
-        if m3u8.find('index_{0}_av'.format(opt.q)) != -1:
-           segments = requests.get(m3u8).text
-           nomfich = '{0}_{1}.mp4'.format(liste[indice-1]['code_programme'],liste[indice-1]['id_diffusion'])
-           with open(nomfich, 'wb') as fd:
-             print('> {0} '.format(nomfich),end='')
-             sys.stdout.flush()
-             for seg in segments.splitlines():
+    while True:
+      indice = int(input("Choix => "))
+      if indice == 0:
+        break
+      if indice <= len(liste):
+        js = requests.get(URL2.format(liste[indice-1]['id_diffusion'])).json()
+        url = ''
+        for vid in js['videos']:
+          if vid['format'] == 'm3u8-download':
+            url = vid['url']
+            break
+        liste_m3u8 = requests.get(url).text
+        for m3u8 in liste_m3u8.splitlines():
+          if m3u8.find('index_{0}_av'.format(opt.q)) != -1:
+            segments = requests.get(m3u8).text
+            nomfich = '{0}_{1}.mp4'.format(liste[indice-1]['code_programme'],liste[indice-1]['id_diffusion'])
+            with open(nomfich, 'wb') as fd:
+              print('> {0} '.format(nomfich),end='')
+              sys.stdout.flush()
+              for seg in segments.splitlines():
                 if seg.find('http:') != -1:
                   chunk = requests.get(seg).content
                   print('.',end='')
                   sys.stdout.flush()
                   fd.write(chunk)
-             print(' OK')
+              print(' OK')
